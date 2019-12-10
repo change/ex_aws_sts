97e45df637ca855722150323b5dbff683073cf0c
Dimitri Tishchenko <dtishchenko@change.org>
2019-12-09 16:28:35 -0800

    get role arn and web identity token from env variables

--- lib/ex_aws/sts/auth_cache/assume_role_web_identity_adapter.ex
+++ lib/ex_aws/sts/auth_cache/assume_role_web_identity_adapter.ex
@@ -20,34 +20,47 @@ defmodule ExAws.STS.AuthCache.AssumeRoleWebIdentityAdapter do
     duration = credential_duration_seconds(expiration)
     role_session_name = Map.get(auth, :role_session_name, "default_session")
 
-    assume_role_options =
-      case auth do
-        %{external_id: external_id} -> [duration: duration, external_id: external_id]
-        _ -> [duration: duration]
+    role_arn = System.get_env("AWS_ROLE_ARN")
+    web_identity_token_file = System.get_env("AWS_WEB_IDENTITY_TOKEN_FILE")
+
+    if !is_nil(role_arn) && !is_nil(web_identity_token_file) do
+      assume_role_options =
+        case auth do
+          %{external_id: external_id} -> [duration: duration, external_id: external_id]
+          _ -> [duration: duration]
+        end
+
+      with {:ok, web_identity_token} <- File.read(web_identity_token_file) do
+        assume_role_request =
+          ExAws.STS.assume_role_with_web_identity(
+            role_arn,
+            role_session_name,
+            web_identity_token,
+            assume_role_options
+          )
+
+        assume_role_config = ExAws.Config.new(:sts, source_profile_auth)
+
+        with {:ok, result} <- ExAws.request(assume_role_request, assume_role_config) do
+          %{
+            access_key_id: result.body.access_key_id,
+            secret_access_key: result.body.secret_access_key,
+            security_token: result.body.session_token,
+            role_arn: auth.role_arn,
+            role_session_name: role_session_name,
+            source_profile: auth.source_profile
+          }
+        else
+          {:error, reason} ->
+            {:error, reason}
+        end
+      else
+        {:error, reason} ->
+          {:error, reason}
       end
-
-    assume_role_request =
-      ExAws.STS.assume_role_with_web_identity(
-        auth.role_arn,
-        role_session_name,
-        auth.web_identity_token,
-        assume_role_options
-      )
-
-    assume_role_config = ExAws.Config.new(:sts, source_profile_auth)
-
-    with {:ok, result} <- ExAws.request(assume_role_request, assume_role_config) do
-      %{
-        access_key_id: result.body.access_key_id,
-        secret_access_key: result.body.secret_access_key,
-        security_token: result.body.session_token,
-        role_arn: auth.role_arn,
-        role_session_name: role_session_name,
-        source_profile: auth.source_profile
-      }
     else
-      {:error, reason} ->
-        {:error, reason}
+      {:error,
+       "AWS_ROLE_ARN and AWS_WEB_IDENTITY_TOKEN_FILE must be available in the environment"}
     end
   end
 
